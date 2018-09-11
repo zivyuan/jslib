810f0a8661f9a6866fc7b634c6178a16c2571c4e
ziv <ziv.yuan@gmail.com>
2018-09-11 10:36:53 +0800

    Create functions.js
    Add parseQuery function

---
+++ functions.js
@@ -0,0 +1,28 @@
+/**
+ * Parse query string to object.
+ * Basic array surported.
+ */
+function parseQuery(paramstr) {
+  var urlStr = (paramstr || '').replace(/^[\?#]/, ''),
+  param = {}
+
+  if (urlStr && urlStr != '') {
+    var urlArr = urlStr.split("&")
+
+    for (var i = 0; i < urlArr.length; i++) {
+      var tempArr = urlArr[i].split("="),
+        key = tempArr[0],
+        val = decodeURIComponent(tempArr[1]);
+
+      if (typeof param[key] === 'undefined') {
+        param[key] = val
+      } else if(param[key] instanceof Array) {
+        param[key].push(val)
+      } else {
+        param[key] = [param[key], val]
+      }
+    }
+    return param
+  }
+  return {}
+}
